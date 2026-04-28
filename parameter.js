1a71cb97f6275101290e290c7297578a8026a11a
Mehedi <mehedi.interface@gmail.com>
2026-04-28 22:35:00 +0600

    17-3 Function parameter, handle multiple parameters

---
+++ parameter.js
@@ -0,0 +1,22 @@
+function eatRice() {
+    console.log("Wash hand, cook rice, eat rice");
+    console.log("Put rice in the bowl, eat rice with hands");
+    
+
+}
+
+eatRice(); 
+
+
+function square(number) {
+    return number * number;
+}
+
+console.log(square(5));
+
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+
+console.log(add(10, 20));
