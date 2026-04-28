610a5f0fe443b231301741d994b698d029330a9f
Mehedi <mehedi.interface@gmail.com>
2026-04-28 22:48:33 +0600

    17-4 How Function works and argument vs parameters

---
+++ arguments.js
@@ -0,0 +1,19 @@
+function doubleIt(num) {
+    const double = num * 2; 
+    console.log(double); 
+}
+
+console.log(doubleIt(5));
+
+const num = 55; 
+doubleIt(num);
+
+const money = 112; 
+doubleIt(money);
+
+function difference(num1, num2) {
+    const diff = num1 - num2; 
+    console.log(diff); 
+}
+
+difference(100, 50); 
