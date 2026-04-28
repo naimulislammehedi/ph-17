d557133dde48c9d39ba2306c003208d3acad6e58
Mehedi <mehedi.interface@gmail.com>
2026-04-28 22:08:00 +0600

    git remote add origin https://github.com/naimulislammehedi/ph-17.git

---
+++ function.js
@@ -0,0 +1,17 @@
+const number = 45; 
+
+function fanOffkor() {
+    console.log("Fan is off");
+    console.log("Walking to the kitchen");
+    console.log("Turning on the light");
+}
+
+// function name()
+function brushTeeth() {
+    console.log("pick up the toothbrush");
+    console.log("put toothpaste on the toothbrush");
+    console.log("brush your teeth");
+    console.log("rinse your mouth");
+}
+
+brushTeeth(); 
